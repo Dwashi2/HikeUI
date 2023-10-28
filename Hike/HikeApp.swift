62162751a72083538f5b2ee93736328058146698
Daniel Washington Ignacio <danielvertigo@hotmail.com>
2023-10-28 10:04:03 -0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Daniel Washington Ignacio on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
